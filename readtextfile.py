007e76f6db6bfb9b6592060e1a1bae77d0b95188
soumyamohanty96 <somyamohanty797@gmail.com>
2022-06-06 20:17:18 +0530

    6-6-22

---
+++ readtextfile.py
@@ -0,0 +1,12 @@
+f=open("C:/Users/Soumya Mohanty/Desktop/textfile1.txt.txt","r")
+y=f.read()
+
+f=open("C:/Users/Soumya Mohanty/Desktop/textfile2.txt.txt","w")
+f.write(y)
+f.close()
+
+f=open("C:/Users/Soumya Mohanty/Desktop/textfile2.txt.txt","r")
+print(f.read())
+
+f = open("C:/Users/Soumya Mohanty/Desktop/textfile1.txt.
+       
